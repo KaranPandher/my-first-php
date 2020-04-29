8b76cb70a1367eded59b7ba244986a683b0f53ef
Karan Pandher <karanpandher@live.ca>
2020-04-29 11:46:51 -0600

    Form MAde

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,6 +4,9 @@
         <li>
             <a href = "./index.php">Home</a>
         </li>
+        <li>
+            <a href = "./my-first-php/includes/form.php">Form</a> 
+        </li>
     </ul>
 </nav> 
 <?php // By default anything wirtten in a PPH file wil be sent to the browser ( or whatever client made the request ) See above, we have some HTML outputting, as we didn ot include any PHP tags <?php etc) that would tell PHP otherwise 
--- index.php
+++ index.php
@@ -1,50 +1,6 @@
 <?php
-// Basic Assignment 
-// Echo is used to send message to browswer or output something 
-$myMessage = 'My First PHP!';
-# This is also a comment 
-/* 
-Multi-line comments! 
-Yay! 
-*/
-
-/** 
- * Data Types 
- */
-// String 
-$myString = 'Hello, World!';
-// Integer
-$myInteger = 34;
-// Float / Double 
-$myFloat = 3.14;
-// Boolean
-$myBoolTrue = TRUE;
-$myBoolFalse = false;
-// NULL 
-$myNull = NULL;
-// Object 
-$myObject = new stdClass();
-// Array 
-$myArray = [' First Item', 2, 'third item'];
-$myOtherArray = array(
-    $myString,
-    $myInteger,
-    $myFloat,
-    $myBoolTrue,
-    $myBoolFalse,
-    $myNull,
-    $myObject
-);
-
-$string1 = 'Hello my name is';
-$string2 = 'Michael Jackson. ';
-// We concatenate string using the "." character. ("+" is reserved for addition )
-$concattedString = $string1 . $string2;
-//Difference between single and double quotes 
-$mySingleQuoteHelloString = 'Hi there! My name is $string2';
-//Double quotes are PARSED. PHP checks for variable names to echo out. 
-$myDoubleQuoteHelloString = "Howdy there! My name is $string2";
-
+// The file's code will execute right here in the file. 
+    include './includes/datatypes.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
