22c4c5312557bc692e182bbe2396524d38abbec2
Karan Pandher <karanpandher@live.ca>
2020-04-29 09:52:34 -0600

    New Array

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php 
+// Basic Assignment 
+// Echo is used to send message to browswer or output something 
     $myMessage = 'My First PHP!';
+# This is also a comment 
+/* 
+Multi-line comments! 
+Yay! 
+*/ 
+
+/** 
+ * Data Types 
+ */
+// String 
+$myString = 'Hello, World!';
+// Integer
+$myInteger = 34; 
+// Float / Double 
+$myFloat = 3.14; 
+// Boolean
+$myBoolTrue = TRUE; 
+$myBoolFalse = false;
+// NULL 
+$myNull = NULL; 
+// Object 
+$myObject = new stdClass(); 
+// Array 
+$myArray = [' First Item', 2, 'third item'];
+$myOtherArray = array(
+        $myString,
+        $myInteger,
+        $myFloat,
+        $myBoolTrue,
+        $myBoolFalse,
+        $myNull,
+        $myObject
+); 
+
 ?> <!DOCTYPE html>
 <html lang = "en">
 <head>
