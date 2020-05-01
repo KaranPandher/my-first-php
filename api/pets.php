c6c47485f4f5e5b803ba66f5b1a637d6e4710b16
Karan Pandher <karanpandher@live.ca>
2020-05-01 10:57:41 -0600

    Pets API

---
+++ api/pets.php
@@ -0,0 +1,50 @@
+<?php
+/** 
+ * This file is an API endpoint. 
+ * Scripts (front end or otherwise) can make requests to this file and recieve a JSON
+ * response. It is up to that script what it will do with the data. 
+ */
+$pets = array(
+  'Bear' => array(
+    'age' => 4,
+    'type' => 'cat',
+    'tricks' => array(
+      'fetch',
+      'roll-over'
+    )
+  ),
+  'Spike' => array(
+    'age' => 16,
+    'type' => 'fish',
+    'tricks' => array(
+      'swim through hoop',
+      'float upside down'
+    )
+  ),
+  'Bingo' => array(
+    'age' => 12,
+    'type' => 'dog',
+    'tricks' => array(
+      'land on all fours',
+      'detect milk from 1 mile away'
+    )
+  ),
+  'Lucas' => array(
+    'age' => 2,
+    'type' => 'newt',
+    'tricks' => array(
+      'hold breath for 4 minutes',
+      'climbing'
+    )
+  )
+); 
+
+// Cleans up how it looks on postman 
+header('Content-Type: application/json');
+
+// Convert from a PHP object/array to a JSON string 
+$petsJSON = json_encode($pets);
+
+// Send this back to whomever (or whatever) made the request
+echo $petsJSON; 
+?>
--- oop.php
+++ oop.php
@@ -10,14 +10,21 @@
    * A basic object.
    */
   $myObject = new stdClass(); // "new" keyword is REQUIRED when making an object from a CLASS (or BLUEPRINT.)
-  // To add a property to our object we use the -> arrow syntax 
-  $myObject ->name = ' Jim Bob-Bob';
-  $myObject ->age = 69; 
-  $myObject ->interests = array('hockey', 'skiing', 'jogging');
+  // To add a property to our object, we use the -> arrow syntax.
+  $myObject->name      = 'Jim Bob-Bob';
+  $myObject->age       = 41;
+  $myObject->interests = array( 'PHP', 'CSS' );
   /**
-   * Inclue our class / blueprint file, so we can use our class. 
+   * Include our class / blueprint file, so we can use our class.
    */
-  include './includes/snack.class.php';
+  include './includes/Snack.Class.php';
+  // Let's make a snack...
+  $cheetos       = new Snack( 'Cheetos', 3.99, 'Chip' );
+  $gushers       = new Snack( 'Fruit Gushers', 2.56, 'Fruit' );
+  $jollyRanchers = new Snack( 'Jolly Ranchers', 1.25, 'Fruit' );
+  $sharwarma     = new Snack( 'Sharwarma', 7.86, 'Wrap' );
+  // Let's throw them in an array for easy output...
+  $snacks = array( $cheetos, $gushers, $jollyRanchers, $sharwarma );
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,5 +37,15 @@
   <?php include './includes/navigation.php'; ?>
   <h2>$myObject dump:</h2>
   <pre><?php var_dump( $myObject ); ?></pre>
+  <h2>Snacks</h2>
+  <?php if ( count( $snacks ) > 0 ) : ?>
+    <ul>
+      <?php foreach ( $snacks as $snack ) : ?>
+        <li>
+          <?php $snack->output( TRUE ); // Run our method! It echoes the snack for us :) ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
 </body>
 </html>
